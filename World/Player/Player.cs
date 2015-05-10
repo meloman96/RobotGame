d30314c1ba87b9cfdcd5f6d37f58354e77015eca
meloman96 <zubavlenkov@gmail.com>
2015-05-10 21:23:17 +0300

    Update Player.cs

--- World/Player/Player.cs
+++ World/Player/Player.cs
@@ -6,10 +6,57 @@ using GameProject.World.Entity;
 
 namespace GameProject.World.Player
 {
-    class Player : EntityAbstract
     {
-        private double Speed = 0.2;
-
-        public double Current_Points = 1;
+        //private double Speed = 0.2;
+        //public double Current_Points = 1;
+        int Health = 3;
+        int[,] WallPosition = new int[100,100];//здесь нужно иметь 2-мерный массив интов, содержащий информацию о стенах в лабиринте, 
+                                                //0-нет, 1-есть
+                                                //предлагаю инициализировать в начале каждого уровня в зависимости от типа карты
+        int PositionX;
+        int PositionY;
+        int NextStep=0;//0-никуда, 1-следющий шаг влево, 2-вверх, 3-вправо, 4-вниз
+        void CheckingWall(int NextStep) //данный метод описывает взаимодействие робота и стен через переменную NextStep, 
+                                        //указывающую направление следующего шага, и массив WallPosition, 
+                                        //который соотнесен с текстуркой карты и показывает наличие/отсуствие стены
+        {
+            switch(NextStep)
+                {
+                case 1 :
+                        if (WallPosition[PositionX - 1, PositionY] == 1)
+                        {
+                            NextStep = 0;
+                            Health--;
+                            //Здесь нужно высветить, что уменьшилось хп и ты врезался в стену
+                        }
+                    break;
+                case 2 :
+                    if (WallPosition[PositionX, PositionY + 1] == 1)
+                    {
+                        NextStep = 0;
+                        Health--;
+                        //Здесь нужно высветить, что уменьшилось хп и ты врезался в стену
+                    }
+                    break;
+                case 3:
+                    if (WallPosition[PositionX + 1, PositionY] == 1)
+                    {
+                        NextStep = 0;
+                        Health--;
+                        //Здесь нужно высветить, что уменьшилось хп и ты врезался в стену
+                    }
+                    break;
+                case 4:
+                    if (WallPosition[PositionX, PositionY - 1] == 1)
+                    {
+                        NextStep = 0;
+                        Health--;
+                        //Здесь нужно высветить, что уменьшилось хп и ты врезался в стену
+                    }
+                    break;
+                default:
+                    break;
+                }
+        }
     }
 }
